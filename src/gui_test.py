46a82b4146b3f53ab4705271b572d203604d0b28
mategyerek <materadio@gmail.com>
2024-09-07 15:47:52 +0200

    simple button example (sucks)

--- src/gui_test.py
+++ src/gui_test.py
@@ -1,1 +1,55 @@
-print("hello world")
+from dash import Dash, dcc, html, Input, Output, State, callback
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Button('up', id='up', n_clicks=0),
+    html.Button('down', id='down', n_clicks=0),
+    html.Button('left', id='left', n_clicks=0),
+    html.Button('right', id='right', n_clicks=0),
+    html.Div(id='output')
+])
+
+
+@callback(
+    Output('output', 'children', allow_duplicate=True),
+    Input('up', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def update_output(n_clicks):
+    print('up')
+    return ''
+
+
+@callback(
+    Output('output', 'children', allow_duplicate=True),
+    Input('down', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def update_output(n_clicks):
+    print('down')
+    return ''
+
+
+@callback(
+    Output('output', 'children', allow_duplicate=True),
+    Input('left', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def update_output(n_clicks):
+    print('left')
+    return ''
+
+
+@callback(
+    Output('output', 'children', allow_duplicate=True),
+    Input('right', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def update_output(n_clicks):
+    print('right')
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

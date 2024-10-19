9f0a3934e271948faa193e1be3e506a375626495
mategyerek <materadio@gmail.com>
2024-10-19 13:06:00 +0200

    colliding blocks improvement

--- utils.js
+++ utils.js
@@ -5,4 +5,3 @@ A file reserved for global utilities shared across applets
 function rms(a, b) { // fastest 2d root mean square according to current benchmarks, should be used everywhere
     return (a * a + b * b) ** 0.5;
 }
-
